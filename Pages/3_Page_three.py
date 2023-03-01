5f7b4f8b4e07b47fc7b174d17c3787a8d318077a
Camille <1015018907@QQ.com>
2023-03-01 22:57:25 +0800

    Add files via upload

---
+++ Pages/3_Page_three.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+#隐藏streamlit自带的功能
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+st.title("Page Three")
